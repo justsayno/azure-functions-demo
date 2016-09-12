014ff2283cd4d557b96bd0b8dab811d2ae0369f0
Seth Reid <contact@sethreid.co.nz>
2016-09-12 19:47:01 +1200

    updaed deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,5 @@
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST ".\package.json" (
   call :ExecuteCmd !NPM_CMD! install --development
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+  IF !ERRORLEVEL! NEQ 0 goto ERRORLEVEL
 )
 call :ExecuteCmd !NPM_CMD! run build
