565f8239741e0ee700e81d618d7cf3a45ad51a45
Seth Reid <contact@sethreid.co.nz>
2016-09-12 19:45:46 +1200

    fixed build.js

--- build.js
+++ build.js
@@ -1,41 +1,33 @@
-var npm = require('npm')
+require('shelljs/global');
 
 if (!which('npm')) {
   echo('Sorry, this script requires npm')
   exit(1)
 }
 
-npm.load(myConfigObject, function (er) {
-  if (er) return handlError(er)
-  npm.commands.install([], function (er, data) {
-  if (er) throw 'Failed to install packages'
+const fs = require('fs');
+const path = require('path');
+const scriptDirectory = __dirname
 
-  require('shelljs/global');
-  const fs = require('fs');
-  const path = require('path');
-  const scriptDirectory = __dirname
+const installPackagesInDirectory = (directoryPath, scriptDirectory) => {
+    if (fs.existsSync(`./${directoryPath}/package.json`)) { 
+        cd(`./${directoryPath}`)
+        exec('npm install')
+        cd(scriptDirectory)
+    }
+}
 
-  const installPackagesInDirectory = (directoryPath, scriptDirectory) => {
-      if (fs.existsSync(`./${directoryPath}/package.json`)) { 
-          cd(`./${directoryPath}`)
-          exec('npm install')
-          cd(scriptDirectory)
-      }
-  }
+getDirectories = (directoryPath) => {
+  return fs.readdirSync(directoryPath).filter(function(file) {
+    return fs.statSync(path.join(directoryPath, file)).isDirectory()
+  });
+}
 
-  getDirectories = (directoryPath) => {
-    return fs.readdirSync(directoryPath).filter(function(file) {
-      return fs.statSync(path.join(directoryPath, file)).isDirectory()
-    });
-  }
+const directories = getDirectories(scriptDirectory)
 
-  const directories = getDirectories(scriptDirectory)
+installPackagesInDirectory(scriptDirectory)
 
-  installPackagesInDirectory(scriptDirectory)
-
-  directories.map((directory) => {
-      installPackagesInDirectory(directory, scriptDirectory)
-  })
-  })
+directories.map((directory) => {
+    installPackagesInDirectory(directory, scriptDirectory)
 })
 
