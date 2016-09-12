7995f2124cda3990bf8205d3d9b3d9d2988880c1
Seth Reid <contact@sethreid.co.nz>
2016-09-13 11:50:50 +1200

    updated build scripts

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,2 @@
-IF EXIST ".\package.json" (
-  call :ExecuteCmd !NPM_CMD! install --development
-  IF !ERRORLEVEL! NEQ 0 goto ERRORLEVEL
-)
+call :ExecuteCmd !NPM_CMD! install --development
 call :ExecuteCmd !NPM_CMD! run build
