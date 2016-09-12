eec9a521d09fb10834c4b9a8b707fb0776112091
Seth Reid <contact@sethreid.co.nz>
2016-09-12 20:06:52 +1200

    updaed

--- rss-processor/index.js
+++ rss-processor/index.js
@@ -8,7 +8,7 @@ const insertRssItemIntoTableStorage = (rssItem, tableName) => {
     return insertPodcastEntity(entity, tableName, storageAccountName, storageAccountKey)
 }
 
-module.exports = function(context, myQueueItem) {
-    context.log('Node.js ServiceBus queue trigger function processed message', myQueueItem);
+module.exports = function (context, myQueueItem) {
+    context.log('Node.js queue trigger function processed work item', myQueueItem);
     context.done();
 };
