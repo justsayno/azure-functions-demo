4ef94a377809769866a7595b19cb777e56538b37
Seth Reid <contact@sethreid.co.nz>
2016-09-12 19:07:06 +1200

    changed to cmd build

---
+++ build.cmd
@@ -0,0 +1,8 @@
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --development
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+call :ExecuteCmd !NPM_CMD! run build
